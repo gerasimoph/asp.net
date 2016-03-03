2f6a0c6939c24d0c4bcf0eab847ad0099e39ec0c
moontag <aligras@ya.ru>
2016-03-04 00:21:12 +0300

    Added site

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+// using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Photos()
+        {
+            return View();
+        }
+        public ActionResult Useful()
+        {
+            return View();
+        }
+        public ActionResult WhatFor()
+        {
+            return View();
+        }
+    }
+}
