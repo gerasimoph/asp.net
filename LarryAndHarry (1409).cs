4c9102dd00649bbb9dc62b7fc5ef8743d1a33d85
moontag <aligras@ya.ru>
2016-03-10 21:14:49 +0300

    Задачка "1409. Два бандита"
    Бандиты Гарри и Ларри отдыхали на природе. Решив пострелять, они
    выставили на бревно несколько банок из-под пива (не больше 10). Гарри
    начал простреливать банки по порядку, начиная с самой левой, Ларри — с
    самой правой. В какой-то момент получилось так, что они одновременно
    прострелили одну и ту же последнюю банку.
    Гарри возмутился и сказал, что Ларри должен ему кучу денег за то, что
    тот лишил его удовольствия прострелить несколько банок. В ответ Ларри
    сказал, что Гарри должен ему еще больше денег по тем же причинам. Они
    стали спорить кто кому сколько должен, но никто из них не помнил сколько
    банок было в начале, а искать простреленные банки по всей округе было
    неохота. Каждый из них помнил только, сколько банок прострелил он сам.
    Определите по этим данным, сколько банок не прострелил Гарри и сколько
    банок не прострелил Ларри.

---
+++ LarryAndHarry (1409).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько банок выбил Гарри?");
+            int HarryStrike = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("А Ларри?");
+            int LarryStrike = Convert.ToInt32(Console.ReadLine());
+            while (HarryStrike+LarryStrike>10 || HarryStrike == 0 || LarryStrike == 0 || HarryStrike < 0 || LarryStrike < 0)
+            {
+                Console.WriteLine("У них было только 10 банок, как не было ни нуля, ни минус-банок :(");
+                Console.WriteLine("Сколько банок выбил Гарри?");
+                HarryStrike = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("А Ларри?");
+                LarryStrike = Convert.ToInt32(Console.ReadLine());
+            }
+            int HarryDontStrike = LarryStrike - 1;
+            int LarryDontStrike = HarryStrike - 1;
+            Console.WriteLine("Гарри не выбил {0} банок, Ларри - {1}", HarryDontStrike, LarryDontStrike);
+        }
+    }
+}
