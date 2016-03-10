7934a3ae34e3980b8f9be542a7ee604daa48034e
moontag <aligras@ya.ru>
2016-03-10 22:01:31 +0300

    Задачка "1068. Сумма"
    Всё, что от вас требуется — найти сумму всех целых чисел, лежащих между
    1 и N включительно.

---
+++ NumbersSum (1068).cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numResult = 0;
+            Console.WriteLine("Введите число");
+            int numFirst = Convert.ToInt32(Console.ReadLine());
+            if (numFirst > 1)
+            {
+                for (int i = numFirst; i >= 1; i--)
+                {
+                    numResult = numResult + i;
+                }
+            }
+            if (numFirst < 1)
+            {
+                while (numFirst <= 1)
+                {
+                    numResult = numResult + numFirst;
+                    numFirst++;
+                }
+            }
+            Console.WriteLine("Сумма чисел до 1 есть {0}", numResult);
+        }
+    }
+}
