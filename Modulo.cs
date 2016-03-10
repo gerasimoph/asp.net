cd38356ee893fa61d80551b3446e7b2fb6b54393
moontag <aligras@ya.ru>
2016-03-10 22:46:27 +0300

    Деление числа на разряды
    Задачка с http://cppstudio.com/post/2591/
    
    Составить программу, которая будет считывать введённое пятизначное
    число. После чего, каждую цифру этого числа необходимо вывести в новой
    строке.

---
+++ Modulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int buff = 1;
+            int k = 5;
+            Console.WriteLine("Введите число:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num>99999||num<10000)
+            {
+                Console.WriteLine("Введите пятизначное число:");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i=0; i < 5; i++)
+            {
+                Console.WriteLine("{0} место занимает цифра {1}", k--, (num / buff) % 10);
+                buff = buff * 10;
+            }
+        }
+    }
+}
